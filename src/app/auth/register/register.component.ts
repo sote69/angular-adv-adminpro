19b07146f76b2b5e6f2981d2ccdd262b05d536c1
David Fernández <fernandezsotelo@gmail.com>
2023-09-16 13:05:17 +0200

    Inicio del login de usuarios

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  private formSubmited = false;
+  private fb = inject(FormBuilder);
+  public registerForm = this.fb.group({
+    nombre:    ['', [ Validators.required, Validators.minLength(3) ]],
+    email:     ['', [ Validators.required ]],
+    password:  ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required, Validators.minLength(6) ]],
+    terminos:  [true, [ Validators.required, Validators.minLength(3) ]],
+  });
+
+  crearUsuario() {
+    this.formSubmited = true;
+    console.log(this.registerForm);
+  }
 }
---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,15 @@
+
+export class Usuario {
+
+  constructor(nombre :String,
+              email :String ,
+              img :String,
+              uid? :String,
+              rol? :String,
+              google? :boolean,
+  )
+  {
+
+  }
+
+};
