7a7518478e69c38f4981a1a57a9d524a8f211d9b
David Fernández <fernandezsotelo@gmail.com>
2023-12-29 15:15:09 +0100

    Añadir iterceptor

---
+++ src/app/interceptors/http-interceptor.service.ts
@@ -0,0 +1,30 @@
+import { HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HttpInterceptorService implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) :Observable<any> {
+
+    console.log('Pasamos por el interceptor....');
+    console.log(req.url);
+
+    const cloneRequest = req.clone({ headers: this.headers });
+
+    return next.handle(cloneRequest).pipe(
+      // catchError(this.manejarError)
+    );
+  }
+
+  get token() :string {
+    return localStorage.getItem('token') || '';
+  }
+
+  get headers() :HttpHeaders {
+    return new HttpHeaders( { "Authorization": `Bearer ${ this.token }` } );
+  }
+}
